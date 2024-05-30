ba91af5f61efd73c9d52cdcbee9219644f454205
Marek Koláč <marekkolac@gmail.com>
2024-05-30 15:33:13 +0200

    makePayment with intent

--- src/api/stripe/controllers/stripe.ts
+++ src/api/stripe/controllers/stripe.ts
@@ -2,7 +2,9 @@
 const stripe = require('stripe')('sk_test_51MWvwTJCZOkBLZhGvWuWQWGjoqZsXt9JWBEyw4Qs27A9CB9BRGsJwcnPpAqxyGbFEJJdNULllALM07A3UJyBmldW004wXY9pG8');
 
 export default {
-  createCheckoutSession: async (ctx, next) => {
+  createCheckoutSession: async (ctx: any, next: any) => {
+    const { productName, price } = ctx.request.body;
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [
@@ -10,18 +12,20 @@ export default {
           price_data: {
             currency: 'czk',
             product_data: {
-              name: 'CERTIFIKÁT',
-              description: 'Lorem ipsum dolor sit amet'
+              name: productName,
+              description: 'Todo: Lorem ipsum dolor sit amet'
             },
-            unit_amount: 180000,
+            unit_amount: price*100,
           },
           quantity: 1,
         },
       ],
       mode: 'payment',
       // success_url: 'http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}',
-      success_url: 'http://localhost:3000/payment/success',
-      cancel_url: 'http://localhost:3000/payment/failed',
+      // success_url: `${process.env.CAPABOOST_URL}/app/order/success`,
+      // cancel_url: `${process.env.CAPABOOST_URL}/app/order/failed`,
+      success_url: 'http://localhost:1234/app/order/success',
+      cancel_url: 'http://localhost:1234/app/order/failed'
     });
 
 
