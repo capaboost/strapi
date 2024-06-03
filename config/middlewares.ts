f4617f5e8e28e779c470111cb824d3f547773749
marekkolac <marekkolac@gmail.com>
2024-06-03 14:17:27 +0200

    middleware update 2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,5 +1,3 @@
-import rawBodyMiddleware from "../src/middlewares/rawBody";
-
 export default [
   'strapi::logger',
   'strapi::errors',
@@ -12,8 +10,7 @@ export default [
   'strapi::favicon',
   'strapi::public',
   {
-    name: 'global::rawBody',
     resolve: './src/middlewares/rawBody',
-    config: {},
+    options: {},
   },
 ];
