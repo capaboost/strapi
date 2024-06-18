17401bde9096a2ea70c811b8c27a1813b97a0811
Marek Koláč <marekkolac@gmail.com>
2024-06-18 15:11:03 +0200

    BASIC FLOW done

--- src/api/user-test/controllers/user-test.ts
+++ src/api/user-test/controllers/user-test.ts
@@ -12,10 +12,10 @@ export default factories.createCoreController('api::user-test.user-test', ({ str
         return ctx.unauthorized('No user, cant save a user test.');
       }
 
-      const { type, testUid, validTo, data } = ctx.request.body.data;
+      const { type, testUid, validTo, data, orderId } = ctx.request.body.data;
 
-      if (!type || !testUid || !validTo || !data) {
-        return ctx.badRequest('All parameters (type, uid, validTo, data) are required');
+      if (!type || !testUid || !validTo || !data || !orderId) {
+        return ctx.badRequest('All parameters (type, testUid, validTo, data, orderId) are required');
       }
 
       try {
@@ -29,7 +29,16 @@ export default factories.createCoreController('api::user-test.user-test', ({ str
           },
         });
 
-        console.log('is success');
+        const userOrders = await strapi.service('api::user-order.user-order').myOrders(ctx);
+        const userOrder = userOrders.find(order => order.id === orderId);
+        if (!userOrder) {
+          return ctx.forbidden('Order not found or does not belong to the user');
+        }
+        await strapi.entityService.update('api::user-order.user-order', orderId, {
+          data: {
+            status: 'COMPLETED',
+          },
+        });
 
         ctx.send({
           newUserTest,
@@ -40,10 +49,6 @@ export default factories.createCoreController('api::user-test.user-test', ({ str
           error,
         });
       }
-
-      ctx.send({
-        msg: 'hello world',
-      });
     } catch(err) {
       // todo
     }
